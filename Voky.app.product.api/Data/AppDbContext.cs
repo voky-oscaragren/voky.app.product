956bb615b6f720a905d0f2161d4b791c3d495456
Oscar Ågren <oscar.agren@voky.com>
2026-06-15 12:06:28 +0200

    Adds controllers and services for all entities

--- Voky.app.product.api/Data/AppDbContext.cs
+++ Voky.app.product.api/Data/AppDbContext.cs
@@ -6,4 +6,12 @@ namespace Voky.app.product.api.Data;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<Product> Products => Set<Product>();
+    public DbSet<MainSupplier> MainSuppliers => Set<MainSupplier>();
+    public DbSet<Lifecycle> Lifecycles => Set<Lifecycle>();
+    public DbSet<SupplierCurrency> SupplierCurrencies => Set<SupplierCurrency>();
+    public DbSet<CurrencyEndPrice> CurrencyEndPrices => Set<CurrencyEndPrice>();
+    public DbSet<QuestionGroup> QuestionGroups => Set<QuestionGroup>();
+    public DbSet<Question> Questions => Set<Question>();
+    public DbSet<QuestionChoice> QuestionChoices => Set<QuestionChoice>();
+    public DbSet<PriceMatrix> PriceMatrices => Set<PriceMatrix>();
 }
--- Voky.app.product.api/Program.cs
+++ Voky.app.product.api/Program.cs
@@ -13,7 +13,24 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 builder.Services.AddScoped<DbProductService>();
+builder.Services.AddScoped<DbMainSupplierService>();
+builder.Services.AddScoped<DbLifecycleService>();
+builder.Services.AddScoped<DbSupplierCurrencyService>();
+builder.Services.AddScoped<DbCurrencyEndPriceService>();
+builder.Services.AddScoped<DbQuestionGroupService>();
+builder.Services.AddScoped<DbQuestionService>();
+builder.Services.AddScoped<DbQuestionChoiceService>();
+builder.Services.AddScoped<DbPriceMatrixService>();
+
 builder.Services.AddScoped<ProductService>();
+builder.Services.AddScoped<MainSupplierService>();
+builder.Services.AddScoped<LifecycleService>();
+builder.Services.AddScoped<SupplierCurrencyService>();
+builder.Services.AddScoped<CurrencyEndPriceService>();
+builder.Services.AddScoped<QuestionGroupService>();
+builder.Services.AddScoped<QuestionService>();
+builder.Services.AddScoped<QuestionChoiceService>();
+builder.Services.AddScoped<PriceMatrixService>();
 
 var app = builder.Build();
 
